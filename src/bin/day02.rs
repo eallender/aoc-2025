39142c5404d008ddcb0bfa0426837ad0e6e87d82
Evan <evan.allender@gmail.com>
2025-12-02 17:44:20 -0500

    Solution: Day 2 Problem 2

--- src/bin/day02.rs
+++ src/bin/day02.rs
@@ -1,5 +1,3 @@
-use std::ascii::AsciiExt;
-
 use aoc_2025::{Args, read_lines};
 use clap::Parser;
 use dotenv::dotenv;
@@ -30,6 +28,49 @@ fn part_1(input: Vec<String>) -> u64 {
     total
 }
 
+fn check_repeating(id: String) -> bool {
+    let mut repeating: bool = false;
+    let mut target: String = "".to_string();
+    for (i, c) in id.chars().enumerate() {
+        target.push(c);
+        if (id.len() % (i + 1)) == 0 && i + 1 != id.len() {
+            repeating = true;
+            for idx in 0..(id.len() / (i + 1)) {
+                let curr: &str = &id[idx * (i + 1)..idx * (i + 1) + i + 1];
+                if target != curr {
+                    repeating = false
+                }
+            }
+            if repeating == true {
+                return repeating;
+            }
+        }
+    }
+    repeating
+}
+
+fn part_2(input: Vec<String>) -> u64 {
+    let id_range: Vec<String> = input[0].split(",").map(|s: &str| s.to_string()).collect();
+    let mut total: u64 = 0;
+
+    for range in id_range.iter() {
+        if let Some((start, end)) = range.split_once("-") {
+            let start_idx: u64 = start.parse().unwrap();
+            let end_idx: u64 = end.parse().unwrap();
+            for id in start_idx..=end_idx {
+                let id_str = id.to_string();
+                if check_repeating(id_str.clone()) {
+                    debug!("Found repeating: {}", id_str);
+                    total += id
+                }
+            }
+        }
+    }
+
+    info!("Total: {}", total);
+    total
+}
+
 fn main() {
     dotenv().ok();
     env_logger::init();
@@ -45,6 +86,9 @@ fn main() {
         }
         Some(2) => {
             info!("-- Executing Part 2 --");
+            if let Ok(input) = read_lines(&args.input) {
+                part_2(input);
+            }
         }
         _ => error!("Insufficient part provided: 1 or 2 required"),
     }
@@ -61,5 +105,19 @@ fn part1_test1() {
 fn part1_test2() {
     if let Ok(input) = read_lines("./inputs/day02/input.txt") {
         assert_eq!(part_1(input), 16793817782);
+    }
+}
+
+#[test]
+fn part2_test1() {
+    if let Ok(input) = read_lines("./inputs/day02/test.txt") {
+        assert_eq!(part_2(input), 4174379265);
+    }
+}
+
+#[test]
+fn part2_test2() {
+    if let Ok(input) = read_lines("./inputs/day02/input.txt") {
+        assert_eq!(part_2(input), 27469417404);
     }
 }
