401d5586806fd497a55c5d58d9ba12b3d365c880
Evan <evan.allender@gmail.com>
2025-12-03 19:24:56 -0500

    Solution: Day 3 Problem 1

--- src/bin/day03.rs
+++ src/bin/day03.rs
@@ -9,14 +9,14 @@ fn part_1(input: Vec<String>) -> u32 {
     for line in input {
         let mut battery1: u32 = 0;
         let mut battery2: u32 = 0;
-        let last = line.chars().last();
-
-        for char in line.chars() {
-            info!("battery1: {battery1}");
-            info!("battery2: {battery2}");
-            info!("current battery: {char}");
+        let last = line.chars().count() - 1;
+        debug!("Current battery bank:\n{}", line);
+        for (i, char) in line.chars().enumerate() {
             if let Some(curr) = char.to_digit(10) {
-                if curr > battery1 && Some(char) != last {
+                debug!("battery1: {}", battery1);
+                debug!("battery2: {}", battery2);
+                debug!("Current: {}", curr);
+                if curr > battery1 && i != last {
                     battery1 = curr;
                     battery2 = 0;
                 } else if curr > battery2 {
@@ -26,8 +26,7 @@ fn part_1(input: Vec<String>) -> u32 {
         }
 
         let joltage = battery1 * 10 + battery2;
-        info!("Current battery bank:\n{}", line);
-        info!("Highest joltage: {}", joltage);
+        debug!("Highest joltage: {}", joltage);
         total_joltage += joltage;
     }
     info!("Total Joltage: {}", total_joltage);
@@ -63,5 +62,31 @@ fn main() {
 fn part1_test1() {
     if let Ok(input) = read_lines("./inputs/day03/test.txt") {
         assert_eq!(part_1(input), 357);
+    }
+}
+#[test]
+fn part1_inc() {
+    assert_eq!(part_1(vec!["1234567".to_string()]), 67);
+}
+
+#[test]
+fn part1_dec() {
+    assert_eq!(part_1(vec!["7654321".to_string()]), 76);
+}
+
+#[test]
+fn part1_lhl() {
+    assert_eq!(part_1(vec!["1234321".to_string()]), 43);
+}
+
+#[test]
+fn part1_hlh() {
+    assert_eq!(part_1(vec!["4321234".to_string()]), 44);
+}
+
+#[test]
+fn part1_input() {
+    if let Ok(input) = read_lines("./inputs/day03/input.txt") {
+        assert_eq!(part_1(input), 17766);
     }
 }
