b8e02e7651b81ab6999847d0c75c3cef64c21ce1
Evan Allender <evan.allender@gmail.com>
2025-12-07 15:10:40 -0500

    Solution: Day 7 Problem 2

--- src/bin/day07.rs
+++ src/bin/day07.rs
@@ -2,6 +2,7 @@ use aoc_2025::{Args, read_lines, create_grid};
 use clap::Parser;
 use dotenv::dotenv;
 use log::{debug, error, info};
+use std::collections::HashMap;
 
 fn part_1(input: Vec<String>) -> u32 {
     let mut grid = create_grid(input);
@@ -49,7 +50,60 @@ fn part_1(input: Vec<String>) -> u32 {
     total_splits
 }
 
-fn part_2(input: Vec<String>) {}
+fn count_timelines(
+    y: usize,
+    x: usize,
+    grid: &Vec<Vec<char>>,
+    memo: &mut HashMap<(usize, usize), u64>
+) -> u64 {
+    if y == grid.len() - 1 {
+        return 1;
+    }
+
+    if let Some(&result) = memo.get(&(y, x)) {
+        return result;
+    }
+
+    let result = match grid[y][x] {
+        '.' | '|' => {
+            count_timelines(y + 1, x, grid, memo)
+        }
+        '^' => {
+            let mut total = 0;
+
+            if x > 0 {
+                total += count_timelines(y, x - 1, grid, memo);
+            }
+
+            if x + 1 < grid[0].len() {
+                total += count_timelines(y, x + 1, grid, memo);
+            }
+
+            total
+        }
+        _ => {
+            panic!("Unexpected char in grid: {}", grid[y][x]);
+        }
+    };
+
+    memo.insert((y, x), result);
+    result
+}
+
+fn part_2(input: Vec<String>) -> u64 {
+    let grid = create_grid(input);
+    let mut memo: HashMap<(usize, usize), u64> = HashMap::new();
+
+    let mut total_paths = 0;
+    for x in 0..grid[0].len() {
+        if grid[1][x] == 'S' {
+            total_paths += count_timelines(2, x, &grid, &mut memo);
+        }
+    }
+
+    info!("Total Paths: {}", total_paths);
+    total_paths
+}
 
 fn main() {
     dotenv().ok();
@@ -84,5 +138,17 @@ fn part1_test() {
 fn part1_input() {
     if let Ok(input) = read_lines("./inputs/day07/input.txt") {
         assert_eq!(part_1(input), 1651);
+    }
+}
+#[test]
+fn part2_test() {
+    if let Ok(input) = read_lines("./inputs/day07/test.txt") {
+        assert_eq!(part_2(input), 40);
+    }
+}
+#[test]
+fn part2_input() {
+    if let Ok(input) = read_lines("./inputs/day07/input.txt") {
+        assert_eq!(part_2(input), 108924003331749);
     }
 }
