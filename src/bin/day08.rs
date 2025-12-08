6c1e254c1689105059baf43d9db19da078b0ed4a
Evan <evan.allender@gmail.com>
2025-12-08 18:44:16 -0500

    Solution: Day 8 Problem 1

--- src/bin/day08.rs
+++ src/bin/day08.rs
@@ -1,4 +1,3 @@
-use core::num;
 use std::collections::HashMap;
 
 use aoc_2025::{Args, read_lines};
@@ -42,7 +41,7 @@ impl Circuit {
     }
 }
 
-fn part_1(input: Vec<String>) -> usize {
+fn part_1(input: Vec<String>, num_conn: usize) -> usize {
     let mut points: Vec<Point> = Vec::new();
     for point in input {
         let parts: Vec<i64> = point
@@ -74,27 +73,46 @@ fn part_1(input: Vec<String>) -> usize {
     let mut circuits: Vec<Circuit> = Vec::new();
     let mut num_edges = 0;
     for edge in edges {
-        if !con_points.contains_key(&edge.box1) && !con_points.contains_key(&edge.box2) {
-            let mut circuit = Circuit { boxes: Vec::new() };
-            circuit.join(edge.box1);
-            con_points.insert(edge.box1, circuits.len());
-            circuit.join(edge.box2);
-            con_points.insert(edge.box2, circuits.len());
-            circuits.push(circuit);
-        } else if con_points.contains_key(&edge.box1) {
-            let circuit = con_points[&edge.box1];
-            circuits[circuit].join(edge.box2);
-            con_points.insert(edge.box2, circuit);
-        } else if con_points.contains_key(&edge.box2) {
-            let circuit = con_points[&edge.box2];
-            circuits[circuit].join(edge.box1);
-            con_points.insert(edge.box1, circuit);
-        }
+        let box1_circuit = con_points.get(&edge.box1).copied();
+        let box2_circuit = con_points.get(&edge.box2).copied();
+        debug!("Circuits: \n{:?}", circuits);
+
         num_edges += 1;
-        if num_edges == 1000 {
+        if num_edges > num_conn {
             break;
         }
-        info!("Circuits: \n{:?}", circuits)
+
+        match (box1_circuit, box2_circuit) {
+            (None, None) => {
+                let circuit_id = circuits.len();
+                let mut circuit = Circuit { boxes: Vec::new() };
+                circuit.join(edge.box1);
+                circuit.join(edge.box2);
+                con_points.insert(edge.box1, circuit_id);
+                con_points.insert(edge.box2, circuit_id);
+                circuits.push(circuit);
+            }
+            (Some(c1), None) => {
+                circuits[c1].join(edge.box2);
+                con_points.insert(edge.box2, c1);
+            }
+            (None, Some(c2)) => {
+                circuits[c2].join(edge.box1);
+                con_points.insert(edge.box1, c2);
+            }
+            (Some(c1), Some(c2)) => {
+                if c1 == c2 {
+                    continue;
+                } else {
+                    let boxes_to_move = circuits[c2].boxes.clone();
+                    for box_id in boxes_to_move {
+                        circuits[c1].join(box_id);
+                        con_points.insert(box_id, c1);
+                    }
+                    circuits[c2].boxes.clear();
+                }
+            }
+        }
     }
 
     let mut total: usize = 1;
@@ -119,8 +137,14 @@ fn main() {
     match args.part {
         Some(1) => {
             info!("-- Executing Part 1 --");
+            let num_conn: usize;
+            if &args.input == "./inputs/day08/test.txt" {
+                num_conn = 10
+            } else {
+                num_conn = 1000
+            }
             if let Ok(input) = read_lines(&args.input) {
-                part_1(input);
+                part_1(input, num_conn);
             }
         }
         Some(2) => {
@@ -130,5 +154,18 @@ fn main() {
             }
         }
         _ => error!("Insufficient part provided: 1 or 2 required"),
+    }
+}
+
+#[test]
+fn part1_test() {
+    if let Ok(input) = read_lines("./inputs/day08/test.txt") {
+        assert_eq!(part_1(input, 10), 40);
+    }
+}
+#[test]
+fn part1_input() {
+    if let Ok(input) = read_lines("./inputs/day08/input.txt") {
+        assert_eq!(part_1(input, 1000), 50568);
     }
 }
